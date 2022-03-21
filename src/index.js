c21f21893acfb8084583e4316a7f8e2e64ba5b04
Diegoo11 <ynoacamino@gmail.com>
2022-03-21 10:32:36 -0500

    se anadio la logica de la barar de busqueda

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ const merry = () => {
     console.log('gatita bonita');
 };
 merry();
-const btnGif = document.getElementById('btnGif');
 const imgGif = document.getElementById('imgGif');
+const form = document.querySelector('form');
+const text = document.querySelector('input');
 
-function askGif() {
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=JkHEEFcPelmB7FkjO1QLrNAMn9NV61Vy&s=cats', { mode: 'cors' })
+function askGif(busqueda) {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=JkHEEFcPelmB7FkjO1QLrNAMn9NV61Vy&s=${busqueda}`, { mode: 'cors' })
         .then((x) => x.json()).then((y) => { imgGif.src = y.data.images.original.url; });
 }
+askGif();
 
-btnGif.onclick = askGif;
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const search = text.value;
+    askGif(search);
+    form.reset();
+});
